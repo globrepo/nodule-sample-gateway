558f56b9c34430cb7c56a8792797e847f48c3408
Costandino Dufort Moraites <constantine.dm@gmail.com>
2019-09-12 16:35:18 -0400

    Revert "Add basic Create Retrieve and search on Order"

--- src/resources/order/types/index.js
+++
@@ -1,3 +0,0 @@
-export { default as OrderCreateInputType } from './OrderCreateInputType';
-export { default as OrderListType } from './OrderListType';
-export { default as OrderType } from './OrderType';
--- src/transforms/index.js
+++
@@ -1,3 +0,0 @@
-import { bind } from '@globality/nodule-config';
-
-bind('graphql.transforms.toItemList', () => item => ({ items: item ? [item] : [] }));
